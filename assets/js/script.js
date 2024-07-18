4ee3a741825f44ba57dcc8521aa82acf0ad67155
Kelsonobd <kelsonobd@gmail.com>
2024-07-18 19:52:57 -0300

    testando js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const plus = document.querySelector("p");
+
+
+
+function openPlus (){
+    plus.style.visibility = "visibility";
+}
+
+
+function closeMinus(){
+    plus.style.visibility = "hidden"
+}
