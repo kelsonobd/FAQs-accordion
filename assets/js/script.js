3d99c1fd6c99dbe700580d349555ebbaaa1849a6
Kelsonobd <kelsonobd@gmail.com>
2024-07-22 16:09:28 -0300

    add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
-const plus = document.querySelector("p");
-
-
-
-function openPlus (){
-    plus.style.visibility = "visibility";
-}
-
-
-function closeMinus(){
-    plus.style.visibility = "hidden"
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".item");
+  
+    items.forEach(item => {
+      const plusIcon = item.querySelector(".plus");
+      const minusIcon = item.querySelector(".minus");
+      const paragraph = item.querySelector("p");
+  
+      plusIcon.addEventListener("click", () => {
+        paragraph.style.visibility = "visible";
+        plusIcon.style.display = "none";
+        minusIcon.style.display = "inline";
+      });
+  
+      minusIcon.addEventListener("click", () => {
+        paragraph.style.visibility = "hidden";
+        minusIcon.style.display = "none";
+        plusIcon.style.display = "inline";
+      });
+    });
+  });
+  
